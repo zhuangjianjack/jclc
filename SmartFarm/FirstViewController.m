ef3dd737509e726f5f05a4a021f15ff7294c57ef
Ssiswent <liyugu971208@gmail.com>
2019-06-05 19:57:06 +0800

    基本实现接收消息

--- SmartFarm/FirstViewController.m
+++ SmartFarm/FirstViewController.m
@@ -10,6 +10,11 @@
 #import <MQTTClient.h>
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *lblConnect;
+@property (weak, nonatomic) IBOutlet UILabel *lblSubscribe;
+@property (weak, nonatomic) IBOutlet UILabel *lblMessages;
+@property (weak, nonatomic) IBOutlet UIButton *btnConnect;
+@property (weak, nonatomic) IBOutlet UIButton *btnSubscribe;
 
 @property MQTTSession *m_Session;
 
@@ -19,7 +24,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //设置边框颜色
+    self.btnConnect.layer.borderColor = [[UIColor blackColor] CGColor];
+    //设置边框宽度
+    self.btnConnect.layer.borderWidth = 1.0f;
+    //给按钮设置角的弧度
+    self.btnConnect.layer.cornerRadius = 5.0f;
+    //设置背景颜色
+    self.btnConnect.backgroundColor = [UIColor cyanColor];
+    self.btnConnect.layer.masksToBounds = YES;
+    
+    //设置边框颜色
+    self.btnSubscribe.layer.borderColor = [[UIColor redColor] CGColor];
+    //设置边框宽度
+    self.btnSubscribe.layer.borderWidth = 1.0f;
+    //给按钮设置角的弧度
+    self.btnSubscribe.layer.cornerRadius = 5.0f;
+    //设置背景颜色
+    self.btnSubscribe.backgroundColor = [UIColor cyanColor];
+    self.btnSubscribe.layer.masksToBounds = YES;
     
 }
 
@@ -30,9 +54,20 @@
     
     self.m_Session = [[MQTTSession alloc] init];
     self.m_Session.transport = m_Transport;
+    self.m_Session.delegate = self;
+    
     [self.m_Session connectWithConnectHandler:^(NSError *error) {
         // Do some work
-        NSLog(@"连接成功");
+        if(error)
+        {
+            NSLog(@"连接失败");
+            self.lblConnect.text = @"连接失败";
+        }
+        else
+        {
+            NSLog(@"连接成功");
+            self.lblConnect.text = @"连接成功";
+        }
     }];
 }
 
@@ -40,15 +75,22 @@
     [self.m_Session subscribeToTopic:@"test" atLevel:MQTTQosLevelAtLeastOnce subscribeHandler:^(NSError *error, NSArray<NSNumber *> *gQoss) {
         if (error) {
             NSLog(@"订阅失败 %@", error.localizedDescription);
+            self.lblSubscribe.text = @"订阅失败";
         } else {
             NSLog(@"订阅成功 Granted Qos: %@", gQoss);
+            self.lblSubscribe.text = @"订阅成功";
         }
     }];
 }
 
-//- (void)newMessage:(MQTTSession *)session data:(NSData *)data onTopic:(NSString *)topic qos:(MQTTQosLevel)qos retained:(BOOL)retained mid:(unsigned int)mid {
-//    // New message received in topic
-//}
+- (void)newMessage:(MQTTSession *)session data:(NSData *)data onTopic:(NSString *)topic qos:(MQTTQosLevel)qos retained:(BOOL)retained mid:(unsigned int)mid {
+    // New message received in topic
+    NSLog(@"订阅的主题是： %@",topic);
+    //把接收到的内容转为字符串
+    NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"收到的消息是： %@",dataString);
+    self.lblMessages.text = dataString;
+}
 
 
 @end
