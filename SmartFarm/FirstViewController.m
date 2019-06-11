5cbee4685151c2353c9bf5252d4d3b6e12428b36
Ssiswent <liyugu971208@gmail.com>
2019-06-11 14:10:35 +0800

    修改解析的数据格式

--- SmartFarm/FirstViewController.m
+++ SmartFarm/FirstViewController.m
@@ -81,7 +81,7 @@
 }
 
 - (IBAction)SubScribe:(id)sender {
-    [self.m_Session subscribeToTopic:@"test" atLevel:MQTTQosLevelAtLeastOnce subscribeHandler:^(NSError *error, NSArray<NSNumber *> *gQoss) {
+    [self.m_Session subscribeToTopic:@"jcsf/gh/iotdata" atLevel:MQTTQosLevelAtLeastOnce subscribeHandler:^(NSError *error, NSArray<NSNumber *> *gQoss) {
         if (error) {
             NSLog(@"订阅失败 %@", error.localizedDescription);
             self.lblSubscribe.text = @"订阅失败";
@@ -137,26 +137,109 @@
     NSLog(@"订阅的主题是： %@",topic);
     //模拟接收String信息
     //把接收到的内容转为字符串
-//    NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//    NSLog(@"收到的消息是： %@",dataString);
-//    self.lblMessages.text = dataString;
+    NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     
-    id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
-    if ([jsonObj isKindOfClass:[NSDictionary class]]) {
-        //强制转换为 NSDictionary
-        NSDictionary * dic = (NSDictionary *)jsonObj;
-        //将解析出来的数据赋给 UITextView 并设置为追加内容
-        self.lblTemperature.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"温度"]];
-        self.lblSalinity.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"盐分"]];
-        self.lblAir.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"空气湿度"]];
-        self.lblLight.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"光照"]];
-        self.lblSoil.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"土壤湿度"]];
-        self.lblConductivity.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"电导率"]];
-    }
-    else
+//    dataString = [dataString stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    dataString = [dataString stringByReplacingOccurrencesOfString:@" " withString:@""];
+//    dataString = [dataString stringByReplacingOccurrencesOfString:@"\\d+" withString:@"\"$0\""];
+    NSRange range = [dataString rangeOfString:@"Time"];
+    NSString *str1 = [NSString stringWithFormat:@"%@", NSStringFromRange(range)];
+    NSString *str2 = [str1 substringWithRange:NSMakeRange(1, 3)];
+    NSUInteger numMon = (NSUInteger)[str2 integerValue] + 12;
+    NSString *month1 = [dataString substringWithRange:NSMakeRange(numMon, 2)];
+    NSUInteger numDate = (NSUInteger)[str2 integerValue] + 15;
+    NSString *date1 = [dataString substringWithRange:NSMakeRange(numDate, 2)];
+    NSUInteger numHour = (NSUInteger)[str2 integerValue] + 18;
+    NSString *hour1 = [dataString substringWithRange:NSMakeRange(numHour, 2)];
+    NSUInteger numMin = (NSUInteger)[str2 integerValue] + 21;
+    NSString *minute1 = [dataString substringWithRange:NSMakeRange(numMin, 2)];
+    NSUInteger numSec = (NSUInteger)[str2 integerValue] + 24;
+    NSString *second1 = [dataString substringWithRange:NSMakeRange(numSec, 2)];
+    if([month1 hasPrefix:@"0"])
     {
-        NSLog(@"接收格式不正确");
+        NSString *month2;
+        month2 = [month1 substringFromIndex:1];
+        dataString = [dataString stringByReplacingOccurrencesOfString:month1 withString:month2];
     }
+    if([date1 hasPrefix:@"0"])
+    {
+        NSString *date2;
+        date2 = [date1 substringFromIndex:1];
+        dataString = [dataString stringByReplacingOccurrencesOfString:date1 withString:date2];
+    }
+    if([hour1 hasPrefix:@"0"])
+    {
+        NSString *hour2;
+        hour2 = [hour1 substringFromIndex:1];
+        dataString = [dataString stringByReplacingOccurrencesOfString:hour1 withString:hour2];
+    }
+    if([minute1 hasPrefix:@"0"])
+    {
+        NSString *minute2;
+        minute2 = [minute1 substringFromIndex:1];
+        dataString = [dataString stringByReplacingOccurrencesOfString:minute1 withString:minute2];
+    }
+    if([second1 hasPrefix:@"0"])
+    {
+        NSString *second2;
+        second2 = [second1 substringFromIndex:1];
+        dataString = [dataString stringByReplacingOccurrencesOfString:second1 withString:second2];
+    }
+    
+//    dataString = [dataString stringByReplacingOccurrencesOfString:@"06" withString:@"6"];
+    NSLog(@"收到的消息是： %@",dataString);
+    
+    NSData *jsonData = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&err];
+    if(err) {
+                NSLog(@"json解析失败：%@",err);
+                return;
+            }
+    NSLog(@"输出%@",dict);
+//    self.lblMessages.text = dataString;
+
+//    id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+//    NSDictionary *dict = jsonObj[@"Payload"];
+//    NSLog(@"输出%@",dict);
+//    if ([jsonObj isKindOfClass:[NSDictionary class]]) {
+//        NSLog(@"接收格式1");
+//        NSMutableArray *id1 = [(NSDictionary*)[jsonObj objectAtIndex:0] objectForKey:@"Payload"];
+//        self.lblTemperature.text = [NSString stringWithFormat:@"%@",[(NSDictionary*)[id1 objectAtIndex:0] objectForKey:@"Data"]];
+//    }
+//        //强制转换为 NSDictionary
+//        NSDictionary * dic = (NSDictionary *)jsonObj;
+//        //将解析出来的数据赋给 UITextView 并设置为追加内容
+//        self.lblTemperature.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"Payload[0]:Data[0]"]];
+//        self.lblSalinity.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"盐分"]];
+//        self.lblAir.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"Payload[0]:Data[1]"]];
+//        self.lblLight.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"Payload[0]:Data[2]"]];
+//        self.lblSoil.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"土壤湿度"]];
+//        self.lblConductivity.text = [NSString stringWithFormat:@"%@", [dic objectForKey:@"电导率"]];
+//    }
+//    else if([jsonObj isKindOfClass:[NSArray class]])
+//    {
+//        NSLog(@"接收格式2");
+//        NSMutableArray *id1 = [(NSDictionary*)[jsonObj objectAtIndex:0] objectForKey:@"Payload"];
+//
+//        self.lblTemperature.text = [NSString stringWithFormat:@"%@",[(NSDictionary*)[id1 objectAtIndex:0] objectForKey:@"Data"]];
+//    }
+//    else if ([jsonObj isKindOfClass:[NSMutableArray class]])
+//    {
+//        NSLog(@"接收格式3");
+//    }
+//    else if ([jsonObj isKindOfClass:[NSMutableDictionary class]])
+//    {
+//        NSLog(@"接收格式4");
+//    }
+//    else if([jsonObj isKindOfClass:[NSString class]])
+//    {
+//        NSLog(@"接收格式5");
+//    }
+//    else
+//    {
+//        NSLog(@"接收格式不正确");
+//    }
 }
 
 
